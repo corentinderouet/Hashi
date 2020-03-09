9956d32a6007c03052eaeccdbfa1515c8fed5ffa
Alexis <lesys.exia@gmail.com>
2020-03-09 09:00:26 +0100

    Update Action.rb
    METTEZ DES COMMENTAIRES ET DE LA DOC

--- src/Action.rb
+++ src/Action.rb
@@ -1,7 +1,7 @@
 #Classe représentant une Action
 # Une action :
-#le nom de l'action (ajout,suppresion,debut ou fin d'hypothese)
-# le lien en question sur lequel l'action est appliquee
+#le nom de l'action (ajout,suppression,début ou fin d'hypothèse)
+# le lien en question sur lequel l'action est appliquée
 
 class Action
 
@@ -9,16 +9,29 @@ class Action
       attr_accessor:lien
 
       private_class_method :new
-
+      
+      # Constructeur
+	#
+	# === Parametres
+	#
+	# * +action+ => Action à faire
+	# * +lien+ => Lien sur lequel faire l'action
       def initialize(action,lien)
           @action = action
           @lien = lien
       end
 
+      # Redéfinition du constructeur
+	#
+	# === Parametres
+	#
+	# * +action+ => Action à faire
+	# * +lien+ => Lien sur lequel faire l'action
       def Action.creer(action,lien)
           new(action,lien)
       end
 
+      # Redéfinition de l'affichage d'une Action
       def to_s()
           return "#{@action}"
       end
