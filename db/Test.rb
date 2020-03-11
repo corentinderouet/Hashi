ff86c22f1c84e25ca1254a5282c9935a4cdfba93
Lesys <alexis.widmer@bts-malraux.net>
2020-03-11 17:32:55 +0100

    Ajout d'une classe de db/Test.rb et de db/GestionBase.rb pour plus de clarté / TOUTES les méthodes de db/GestionBase.rb sont passées en méthodes de classe

--- db/Test.rb
+++ db/Test.rb
@@ -10,46 +10,52 @@ require_relative "Mode"
 require_relative "PhrasesAventure"
 require_relative "GestionBase"
 
-Difficulte.create( :niveau => "Facile")
-Difficulte.create( :niveau => "Moyen")
-Difficulte.create( :niveau => "Difficile")
-Mode.create( :mode_jeu => "Entrainement")
-Mode.create( :mode_jeu => "Classe")
-Mode.create( :mode_jeu => "Aventure")
+class TestBase
+	def TestBase.test1
+		Difficulte.create( :niveau => "Facile")
+		Difficulte.create( :niveau => "Moyen")
+		Difficulte.create( :niveau => "Difficile")
+		Mode.create( :mode_jeu => "Entrainement")
+		Mode.create( :mode_jeu => "Classe")
+		Mode.create( :mode_jeu => "Aventure")
+		
+		unJoueur = GestionBase.ajouterJoueur("corentin")
+		unJoueur2 = GestionBase.ajouterJoueur("alexis")
+		
+		grille=GrilleDb.create(:grilleSer => "1__5___;", :grilleSolution => "1__5HHH", :niveau =>"Facile", :mode_jeu => "Classe")
+		GrilleDb.create(:grilleSer => "1__5___;", :grilleSolution => "1__5HHH", :niveau =>"Moyen", :mode_jeu => "Classe")
+		GrilleDb.create(:grilleSer => "1__5___;", :grilleSolution => "1__5HHH", :niveau =>"Difficile", :mode_jeu => "Classe")
+		
+		grille4=GrilleDb.create(:grilleSer => "1__5___;", :grilleSolution => "1__5HHH", :niveau =>"Difficile", :mode_jeu => "Entrainement")
+		
+		Joue.create( :joueurs_id => 1, :grille_dbs_id => 1, :score => 2521)
+		Joue.create( :joueurs_id => 1, :grille_dbs_id => 4, :score => 3210)
+		Joue.create( :joueurs_id => 1, :grille_dbs_id => 3, :score => 1)
+		Joue.create( :joueurs_id => 1, :grille_dbs_id => 2, :score => 2)
+		
+		puts(GestionBase.recupJoueur("corentin"))
+		puts "|#{GestionBase.recupJoueur("robin") == nil}|"
+		puts(GestionBase.recupDifficulte(grille.id))
+		puts(GestionBase.recupMode(grille4.id))
+		puts(GestionBase.recupScoreTotal(1))
+		puts(GestionBase.recupNbGrillesJouees(1))
+		puts "Joueur 4 nb: #{GestionBase.recupNbGrillesJouees(4) == nil}"
+		puts(GestionBase.recupGrilles(1))
+		
+		puts GestionBase.recupScoreTotal(1)
+		puts "Grilles: "
+		puts GestionBase.recupGrilles(1)
+		puts "Nil?"
+		puts "|#{GestionBase.recupGrilles(2) == nil}|"
+		puts "|#{GestionBase.recupGrilles(3) == nil}|"
+		#tab=Joue.where([ "joueurs_id = ?", 1 ]).select {|joue| GestionBase.recupMode(GrilleDb.find(joue.grille_dbs_id).id)}# == "Classe"}
+		#puts "Tableau"
+		#puts tab
+		#puts "Somme"
+		#score = tab.select{ |joue| GestionBase.recupMode(GrilleDb.find(joue.grille_dbs_id).id).mode_jeu == "Classe"}
+		#p score
+		#puts score.inject(0) { |score, joue| score += joue.score }
+	end
+end
 
-unJoueur = ajouterJoueur("corentin")
-unJoueur2 = ajouterJoueur("alexis")
-
-grille=GrilleDb.create(:grilleSer => "1__5___;", :grilleSolution => "1__5HHH", :niveau =>"Facile", :mode_jeu => "Classe")
-GrilleDb.create(:grilleSer => "1__5___;", :grilleSolution => "1__5HHH", :niveau =>"Moyen", :mode_jeu => "Classe")
-GrilleDb.create(:grilleSer => "1__5___;", :grilleSolution => "1__5HHH", :niveau =>"Difficile", :mode_jeu => "Classe")
-
-grille4=GrilleDb.create(:grilleSer => "1__5___;", :grilleSolution => "1__5HHH", :niveau =>"Difficile", :mode_jeu => "Entrainement")
-
-Joue.create( :joueurs_id => 1, :grille_dbs_id => 1, :score => 2521)
-Joue.create( :joueurs_id => 1, :grille_dbs_id => 4, :score => 3210)
-Joue.create( :joueurs_id => 1, :grille_dbs_id => 3, :score => 1)
-Joue.create( :joueurs_id => 1, :grille_dbs_id => 2, :score => 2)
-
-puts(recupJoueur("corentin"))
-puts "|#{recupJoueur("robin") == nil}|"
-puts(recupDifficulte(grille.id))
-puts(recupMode(grille4.id))
-puts(recupScoreTotal(1))
-puts(recupNbGrillesJouees(1))
-puts "Joueur 4 nb: #{recupNbGrillesJouees(4) == nil}"
-puts(recupGrilles(1))
-
-puts recupScoreTotal(1)
-puts "Grilles: "
-puts recupGrilles(1)
-puts "Nil?"
-puts "|#{recupGrilles(2) == nil}|"
-puts "|#{recupGrilles(3) == nil}|"
-#tab=Joue.where([ "joueurs_id = ?", 1 ]).select {|joue| recupMode(GrilleDb.find(joue.grille_dbs_id).id)}# == "Classe"}
-#puts "Tableau"
-#puts tab
-#puts "Somme"
-#score = tab.select{ |joue| recupMode(GrilleDb.find(joue.grille_dbs_id).id).mode_jeu == "Classe"}
-#p score
-#puts score.inject(0) { |score, joue| score += joue.score }
+TestBase.test1
