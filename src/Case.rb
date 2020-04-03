fae36f56fa38e1defdd94d3bffcc9bd352fccfe8
lepoittevin <eliot.lepoittevin@gmail.com>
2020-04-03 11:35:35 +0200

    ajout de gestion d'archipelle en plus et autres

--- src/Case.rb
+++ src/Case.rb
@@ -117,6 +117,26 @@ class Case
         return compteur
     end
 
+    # compte le nb de voisins dispo de la case possèdant une certaine étiquette ACTUEL
+    #
+    # === Parametres
+    #
+    # * + etiquette + = > l'etiquette à test
+    # * + tabLien + = > le tableau des liens
+    #
+    # === Retour
+    #
+    # entier sur le nombre de voisins possédant cette etiquette
+    #
+    def voisinsDispoEtiRestanteDe(etiquette,tabLien)
+        compteur=0
+        for i in 0..3 do
+            if(@tabTriangle[i]==true && ( @tabVoisins[i].etiquetteCase.to_i - @tabVoisins[i].nbLienCase(tabLien) )==etiquette)
+                compteur+=1
+            end
+        end
+        return compteur
+    end
 
     # test si un lien est présent pour un voisin dont l'etiquette est différente du parametre OU si le nombre max de voisins avec 
     #l'etiquette est dépassé et qu'il y en a un autre avec cette meme etiquette et qu'il y a un lien
